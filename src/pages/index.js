7a70ea9cee9ee9d1311ecc7d16b33b420f171cf5
Phillip Nguyen <nh273@cornell.edu>
2019-04-11 09:30:38 -0400

    page styling applied

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
+import Page from "../components/Page"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Page>
+    <h1>Let's Try Some Heading</h1>
+    <div>Helloooo world!</div>
+  </Page>
+)
