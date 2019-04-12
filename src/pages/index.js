94aed9605f9523ebeb8c7e9fa92ed40b2cdadb68
Phillip Nguyen <nh273@cornell.edu>
2019-04-12 17:55:14 -0400

    working navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,96 @@
 import React from "react"
 import Page from "../components/Page"
+import Navbar from "../components/Navbar"
 
 export default () => (
   <Page>
+    <Navbar />
     <h1>Let's Try Some Heading</h1>
-    <div>Helloooo world!</div>
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+      aliquam malesuada nunc, ut imperdiet urna condimentum et. Praesent
+      placerat elit in massa rhoncus, vel volutpat risus efficitur. Ut neque
+      ipsum, tincidunt sit amet fringilla nec, tempus ut lorem. Pellentesque sit
+      amet justo ut nisi tincidunt vehicula. Mauris ac pharetra urna. Nam
+      hendrerit urna mi, non porta elit ornare eget. Morbi fermentum, tortor at
+      porta vestibulum, dui lacus vulputate mi, eu maximus enim est ut lorem.
+      Aliquam feugiat augue eu orci blandit luctus. Cras facilisis lacus in
+      auctor tristique. Donec ligula risus, scelerisque a purus vitae, gravida
+      porta libero. Integer pellentesque velit risus, quis volutpat dolor
+      sagittis ut. In hac habitasse platea dictumst. Phasellus sollicitudin
+      tristique orci eu varius. Nulla sem turpis, imperdiet eget felis non,
+      porta facilisis dui. Suspendisse potenti. Vestibulum vel nunc molestie,
+      convallis ligula id, viverra eros. Duis ligula ex, aliquet quis pulvinar
+      nec, porta in augue. Nunc euismod ligula et ex consectetur, sed iaculis
+      velit consectetur. Interdum et malesuada fames ac ante ipsum primis in
+      faucibus. Suspendisse libero sapien, interdum bibendum metus sit amet,
+      sagittis aliquet nisl. Vivamus sed ante egestas, laoreet sem eget,
+      pulvinar elit. Donec viverra pharetra arcu, dictum mattis nibh feugiat ac.
+      Duis quis pulvinar turpis, eu hendrerit tortor. Sed eu mollis purus.
+      Maecenas auctor tincidunt egestas. Aenean commodo mauris orci, quis
+      viverra ante tempus sagittis. Etiam magna massa, tristique ac cursus sed,
+      facilisis vel sem. Nunc quis mi elit. Aenean scelerisque odio nec nunc
+      faucibus volutpat. Sed et feugiat nibh, ac gravida dui. Praesent convallis
+      dui vitae ultrices hendrerit. Donec sapien ante, tempus sit amet nulla
+      vel, maximus suscipit turpis. Aliquam lacus ex, condimentum eget elit vel,
+      lobortis sodales ligula. Praesent facilisis, dui id efficitur consequat,
+      odio dui gravida libero, et viverra odio nisl non justo. Vestibulum
+      dapibus vel metus vitae varius. Vestibulum ante ipsum primis in faucibus
+      orci luctus et ultrices posuere cubilia Curae; Nunc felis lacus, hendrerit
+      non nisi vel, aliquet fermentum odio. Praesent laoreet faucibus nulla a
+      placerat. Integer ut nisi imperdiet, iaculis nunc quis, porttitor massa.
+      Cras sollicitudin eleifend viverra. Ut nec metus sed elit volutpat
+      molestie ac id felis. Curabitur eget mauris faucibus, cursus tellus id,
+      lacinia orci. Integer iaculis magna eget justo vulputate dignissim at ut
+      massa. In dapibus urna sed magna viverra fermentum. Aliquam placerat,
+      magna vel fringilla convallis, dui enim pharetra urna, eget varius eros
+      tellus et risus. Quisque viverra luctus arcu ac luctus. Nunc ut nisl eget
+      nisl ultrices luctus vitae in velit. Pellentesque sapien nisi, suscipit ut
+      ipsum dignissim, mattis lobortis urna. Sed aliquet tristique semper.
+      Quisque eu dolor et elit gravida mattis. Proin blandit ante facilisis
+      turpis semper, a cursus nulla feugiat. Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit. Pellentesque aliquam malesuada nunc, ut
+      imperdiet urna condimentum et. Praesent placerat elit in massa rhoncus,
+      vel volutpat risus efficitur. Ut neque ipsum, tincidunt sit amet fringilla
+      nec, tempus ut lorem. Pellentesque sit amet justo ut nisi tincidunt
+      vehicula. Mauris ac pharetra urna. Nam hendrerit urna mi, non porta elit
+      ornare eget. Morbi fermentum, tortor at porta vestibulum, dui lacus
+      vulputate mi, eu maximus enim est ut lorem. Aliquam feugiat augue eu orci
+      blandit luctus. Cras facilisis lacus in auctor tristique. Donec ligula
+      risus, scelerisque a purus vitae, gravida porta libero. Integer
+      pellentesque velit risus, quis volutpat dolor sagittis ut. In hac
+      habitasse platea dictumst. Phasellus sollicitudin tristique orci eu
+      varius. Nulla sem turpis, imperdiet eget felis non, porta facilisis dui.
+      Suspendisse potenti. Vestibulum vel nunc molestie, convallis ligula id,
+      viverra eros. Duis ligula ex, aliquet quis pulvinar nec, porta in augue.
+      Nunc euismod ligula et ex consectetur, sed iaculis velit consectetur.
+      Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse
+      libero sapien, interdum bibendum metus sit amet, sagittis aliquet nisl.
+      Vivamus sed ante egestas, laoreet sem eget, pulvinar elit. Donec viverra
+      pharetra arcu, dictum mattis nibh feugiat ac. Duis quis pulvinar turpis,
+      eu hendrerit tortor. Sed eu mollis purus. Maecenas auctor tincidunt
+      egestas. Aenean commodo mauris orci, quis viverra ante tempus sagittis.
+      Etiam magna massa, tristique ac cursus sed, facilisis vel sem. Nunc quis
+      mi elit. Aenean scelerisque odio nec nunc faucibus volutpat. Sed et
+      feugiat nibh, ac gravida dui. Praesent convallis dui vitae ultrices
+      hendrerit. Donec sapien ante, tempus sit amet nulla vel, maximus suscipit
+      turpis. Aliquam lacus ex, condimentum eget elit vel, lobortis sodales
+      ligula. Praesent facilisis, dui id efficitur consequat, odio dui gravida
+      libero, et viverra odio nisl non justo. Vestibulum dapibus vel metus vitae
+      varius. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+      posuere cubilia Curae; Nunc felis lacus, hendrerit non nisi vel, aliquet
+      fermentum odio. Praesent laoreet faucibus nulla a placerat. Integer ut
+      nisi imperdiet, iaculis nunc quis, porttitor massa. Cras sollicitudin
+      eleifend viverra. Ut nec metus sed elit volutpat molestie ac id felis.
+      Curabitur eget mauris faucibus, cursus tellus id, lacinia orci. Integer
+      iaculis magna eget justo vulputate dignissim at ut massa. In dapibus urna
+      sed magna viverra fermentum. Aliquam placerat, magna vel fringilla
+      convallis, dui enim pharetra urna, eget varius eros tellus et risus.
+      Quisque viverra luctus arcu ac luctus. Nunc ut nisl eget nisl ultrices
+      luctus vitae in velit. Pellentesque sapien nisi, suscipit ut ipsum
+      dignissim, mattis lobortis urna. Sed aliquet tristique semper. Quisque eu
+      dolor et elit gravida mattis. Proin blandit ante facilisis turpis semper,
+      a cursus nulla feugiat.
+    </div>
   </Page>
 )
